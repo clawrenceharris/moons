63bd44c834584b0f4a31cab9ac9511b3f4a99d63
Caleb Harris <clawrenceharris@gmail.com>
2025-05-24 19:44:14 -0400

    commit

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,9 +1,10 @@
 import mysql2 from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  database: "moons_footwear",
-  password: "",
+  uri: process.env.DATABASE_URL,
+  connectionLimit: 10,
 });
 export { db };
