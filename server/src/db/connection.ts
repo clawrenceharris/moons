b739f53dbc38b5d66285854e5b1f9e70fc4fa065
Caleb Harris <clawrenceharris@gmail.com>
2025-05-24 23:40:20 -0400

    db

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql2.createPool({
-  uri: process.env.DATABASE_URL,
+  uri: process.env.JAWSDB_URL,
   connectionLimit: 10,
 });
 export { db };
